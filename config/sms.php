f4cc6f94cd8bc60b05ceb92b5cea75d3dee3c7b7
ashiri <a.shiri@netafraz.com>
2024-12-31 10:43:07 +0330

    feat: consider sms handler classes in config file to keep solid principals

---
+++ config/sms.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'handlers' => [
+        \App\Services\Sms\Handlers\SmsPanelOne::class,
+        \App\Services\Sms\Handlers\SmsPanelTwo::class,
+        \App\Services\Sms\Handlers\SmsPanelThree::class,
+    ],
+];
