36b9c50487b0f88d65aba5378b5c33480bf01868
ashiri <a.shiri@netafraz.com>
2024-12-31 10:44:12 +0330

    feat: add sms service provider

--- config/app.php
+++ config/app.php
@@ -195,6 +195,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         *  Sms Service Provider
+         */
+        App\Providers\SmsServiceProvider::class,
+
     ],
 
     /*
